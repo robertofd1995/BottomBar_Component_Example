2b1564e5969f622fc2091596693b7bddd360daad
Roberto Fernandez Diaz <robertofd1995@gmail.com>
2017-09-01 16:39:57 +0200

    Example done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {NavTab} from "bottombar-component";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  tabs : NavTab[] = [{
+    "title":"test1",
+    "icon":"fa-facebook",
+    "link":"/test1",
+    "state":"unactive",
+    "backgroundColor" : "red"
+  },
+    {"title":"test2",
+      "icon":"fa-twitter",
+      "link":"/test2",
+      "state":"unactive",
+      "backgroundColor" : "blue"},
+    {"title":"test1",
+      "icon":"fa-medium",
+      "link":"/test2",
+      "state":"unactive",
+      "backgroundColor" : "green"},
+    {"title":"test1",
+      "icon":"fa-medium",
+      "link":"/test2",
+      "state":"unactive",
+      "backgroundColor" : "yellow"}
+      ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
+import { BottomBarModule } from 'bottombar-component';
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
+
+
+export const appRoutes: Routes = [
+  { path: 'test1', component: Test1Component },
+  { path: 'test2',      component: Test2Component },
+  { path: '',
+    redirectTo: '/test1',
+    pathMatch: 'full'
+  },
+  { path: '**', component: Test2Component }
+];
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Test1Component,
+    Test2Component,
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    BrowserAnimationsModule,
+    BottomBarModule.forRoot(),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
